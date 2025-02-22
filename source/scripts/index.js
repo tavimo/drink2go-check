8b3759ebcae5d62f8e0d309d8ee20082d4c60b5f
Tatiana <tavimo14@gmail.com>
2025-02-23 04:35:50 +0500

    Слайдер 1 часть

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,3 +1,4 @@
+/*Меню навигации*/
 const mainHeaderList = document.querySelector('.main-header__list');
 const toggleButton = document.querySelector('.js-toggle-button');
 
@@ -14,3 +15,46 @@ window.addEventListener('resize', () => {
     mainHeaderList.classList.remove('main-header__list--open');
   }
 });
+
+/*Слайдер*/
+// Получаем элементы слайдера
+
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.slider__button--prev');
+const nextButton = document.querySelector('.slider__button--next');
+const slides = Array.from(slider.querySelectorAll('.slider__container'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+
+  prevButton.disabled = slideIndex === 0; // Блокируем кнопку "Назад", если это первый слайд
+  nextButton.disabled = slideIndex === slideCount - 1;
+}
+
+// Инициализация слайдера
+updateSlider();
