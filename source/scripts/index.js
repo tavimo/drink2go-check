48c238f1d04f45cbfe50e20f2717d71b7d22785a
Tatiana <tavimo14@gmail.com>
2025-02-23 01:54:19 +0500

    Открытие меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+const mainHeaderList = document.querySelector('.main-header__list');
+const toggleButton = document.querySelector('.js-toggle-button');
+
+toggleButton.onclick = function() {
+  mainHeaderList.classList.toggle('main-header__list--open');
+  mainHeaderList.classList.toggle('main-header__list--close');
+  toggleButton.classList.toggle('main-header__toggle--close');
+  toggleButton.classList.toggle('main-header__toggle--open');
+};
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    mainHeaderList.classList.add('main-header__list--close');
+    mainHeaderList.classList.remove('main-header__list--open');
+  }
+});
